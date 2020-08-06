f418aa87b1aeb99188dedd59707df09699b7ae4a
Lucas Silva <69328035+Lucas-stk@users.noreply.github.com>
2020-08-06 19:53:20 -0300

    Create programa.py

---
+++ programa.py
@@ -0,0 +1,19 @@
+#Esse script foi feito por Lucas Silva#
+#Não copie e cole! Faça você mesmo que aprenderá melhor!
+
+#Entrada do programa
+print('Bem vindo ao programa\n')
+print('Vamos calcular!\n')
+
+#Entrada de dados
+l = float(input('Digite a largura: '))
+a = float(input('Digite a altura: '))
+ar = l*a
+lat = ar/2
+
+#Saída de dados
+print('Sua dimensão é de {}x{} e sua área é de {:.2f}m²\n'.format(l, a, ar))
+print('Para pintar  tudo, você vai precisar de {}latas de tinta'.format(lat))
+
+print('\n')
+print('########## CODED BY LUCAS SILVA ##########')
